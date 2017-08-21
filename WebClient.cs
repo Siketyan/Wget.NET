db4229a8ce7f50abaa923e2d1cb0be8b69f40438
Siketyan <siketyan@sikeserver.com>
2017-08-21 10:22:17 +0900

    Add WebClient

---
+++ WebClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace Wget
+{
+    public class WebClient : IDisposable
+    {
+        private readonly HttpClient _client;
+
+        public WebClient()
+        {
+            _client = new HttpClient();
+        }
+
+        public void DownloadFile(string src, string dest)
+        {
+            using (var srcStream = _client.GetStreamAsync(src).Result)
+            using (var destStream = new FileStream(dest, FileMode.OpenOrCreate))
+            {
+                srcStream.CopyTo(destStream);
+            }
+        }
+        
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
